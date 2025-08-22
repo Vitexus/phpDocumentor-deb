271f600830cd474a95fb494c38bbd306611c4a1c
CyberVitexus <info@vitexsoftware.cz>
2025-08-22 23:12:38 +0200

    try to build again

--- debian/upver.sh
+++ debian/upver.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-VERSION=$(curl -s https://api.github.com/repos/phpDocumentor/phpDocumentor/releases/latest | grep 'tag_name' | cut -d '"' -f4 | sed 's/v//')
+VERSION=$(curl -s https://api.github.com/repos/phpDocumentor/phpDocumentor/releases/latest | grep '"tag_name"' | head -1 | awk -F: '{gsub(/[", ]/,"",$2); print $2}')
+if [ -z "$VERSION" ]; then
+	echo "Error: Could not fetch latest release version from GitHub." >&2
+	exit 1
+fi
+VERSION=${VERSION#v}
 dch -r $VERSION
 dch -v $VERSION "Updated to $VERSION"
